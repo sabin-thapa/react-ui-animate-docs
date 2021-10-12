fb9a8f9dffdfa8e5dd2c18f8574f7da6e12a6c1c
dipeshrai123 <raidipesh78@gmail.com>
2021-10-12 08:54:18 +0545

    Mounting and Unmounting components complete

--- src/app/pages/animatedValues/animatedValues.page.js
+++ src/app/pages/animatedValues/animatedValues.page.js
@@ -122,13 +122,17 @@ export default function () {
 
         <Paragraph>
           In the above example when we click button, opacity is animated from 0
-          to 1 smoothly.
+          to 1 smoothly. As you can notice <Highlight>AnimatedBlock</Highlight>{" "}
+          is used instead of any other HTML Element because the Animated Values
+          cannot be read by HTML Elements, we need special type of Component
+          that can read it. Here, we have <Highlight>AnimatedBlock</Highlight>{" "}
+          HOC which is a <Highlight>div</Highlight> element and also can read
+          Animated Values.
         </Paragraph>
       </Section>
 
       <Section>
         <SecondaryTitle>Mounting and Unmounting Components</SecondaryTitle>
-
         <Paragraph>
           Second most important aspect of Animated Value is that they provide a
           way to handle mounting and unmounting of any component. Generally, we
@@ -137,20 +141,96 @@ export default function () {
           React UI Animate provides <Highlight>useMountedValue</Highlight> hook
           to handle these scenarios.
         </Paragraph>
-
         <Paragraph>
-          <Highlight>useMountedValue</Highlight> returns a function when a state
-          and phases are passed as first and second arguments. The first
-          argument must be a boolean state and second argument is an array of
-          numbers with three phases <Highlight>from, enter and exit</Highlight>.
           To create Mounted Value, we use <Highlight>useMountedValue</Highlight>{" "}
           hook:
         </Paragraph>
-
         <Code>
           {`const mountedFunction = useMountedValue(boolean, [from, enter, exit]);
 `}
         </Code>
+        <Paragraph>
+          <Highlight>useMountedValue</Highlight> returns a function when a state
+          and phases are passed as first and second arguments. The first
+          argument must be a <Highlight>boolean</Highlight> state and second
+          argument is an array with three phases{" "}
+          <Highlight>from, enter and exit</Highlight>. The phases{" "}
+          <Highlight>from, enter and exit</Highlight> are three numeric values
+          which defines the transition lifecycle of a component when it mounts
+          and unmounts.
+        </Paragraph>
+        <Code>
+          {`import { useState } from "react";
+import { useMountedValue } from "react-ui-animate";
+
+function SomeComponent() {
+  const [visible, setVisible] = useState(false);
+
+  const open = useMountedValue(visible, [0, 1, 0]);
+
+  return (
+    <div>
+      <button 
+        onClick={() => setVisible((prev) => !prev)}
+      >
+        CLICK ME
+      </button>
+    </div>
+  );
+}
+`}
+        </Code>
+        <Paragraph>
+          In the above example, by default the state is{" "}
+          <Highlight>visible = false</Highlight> and the phases are{" "}
+          <Highlight>phases = [ 0, 1, 0 ]</Highlight>. Now, lets use the mounted
+          function <Highlight>open</Highlight> with a component.
+        </Paragraph>
+        <Code>
+          {`import { useState } from "react";
+import { useMountedValue } from "react-ui-animate";
+
+function SomeComponent() {
+  const [visible, setVisible] = useState(false);
+
+  const open = useMountedValue(visible, [0, 1, 0]);
+
+  return (
+    <div>
+      {open(
+        (animation, mounted) =>
+          mounted && (
+            <AnimatedBlock
+              style={{
+                width: 100,
+                height: 100,
+                backgroundColor: "#3399ff",
+                opacity: animation.value
+              }}
+            />
+          )
+      )}
+
+      <button 
+        onClick={() => setVisible((prev) => !prev)}
+      >
+        CLICK ME
+      </button>
+    </div>
+  );
+}
+`}
+        </Code>
+        <Paragraph>
+          In the above example, <Highlight>open</Highlight> function receives a
+          callback that receives two arguments: the Animated Value and a boolean
+          respectively. The first argument, Animated Value animates from
+          phases[0] = 0 to phases[1] = 1 when the visible is true and phases[1]
+          = 1 to phases[2] = 0 when visible is false. And the second argument,
+          boolean dinamically determines whether the component is mounted or not
+          after animation. <Highlight>AnimatedBlock</Highlight> HOC is used to
+          read animated values.
+        </Paragraph>
       </Section>
 
       <NextPrevButton
