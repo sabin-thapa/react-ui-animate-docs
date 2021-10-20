442ab761357dad1cbd8f47b80d78503ea7707c42
dipeshrai123 <raidipesh78@gmail.com>
2021-10-20 14:18:16 +0545

    Code blocks new

--- src/app/commons/code/index.js
+++ src/app/commons/code/index.js
@@ -1,15 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { CopyBlock, dracula } from "react-code-blocks";
 
-import { CopyBlock, tomorrow } from "react-code-blocks";
+import { Pre, Line, LineNo, LineContent } from "./styles";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import theme from "prism-react-renderer/themes/nightOwl";
 
 export const Code = ({ children, language = "jsx" }) => {
+  return (
+    <Highlight
+      {...defaultProps}
+      theme={theme}
+      code={children}
+      language={language}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <Pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            <Line key={i} {...getLineProps({ line, key: i })}>
+              <LineNo>{i + 1}</LineNo>
+              <LineContent>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </LineContent>
+            </Line>
+          ))}
+        </Pre>
+      )}
+    </Highlight>
+  );
+};
+export const CodeOld = ({ children, language = "jsx" }) => {
   return (
     <div className="demo-code">
       <CopyBlock
         language={language}
         text={children}
-        theme={tomorrow}
+        theme={dracula}
         showLineNumbers={true}
         wrapLines={true}
         codeBlock
--- src/app/commons/nextPrevButton/index.js
+++ src/app/commons/nextPrevButton/index.js
@@ -10,51 +10,47 @@ export const NextPrevButton = ({ left, right }) => {
 
   return (
     <div className="nextprev">
-      <Box flexBox>
-        <Box flex={1}>
-          {left && (
-            <button
-              className="nextprev-button left"
-              onClick={() => navigate(left.to)}
-            >
-              <span className="nextprev-button-label">Previous</span>
+      <div className="nextprev-left">
+        {left && (
+          <button
+            className="nextprev-button left"
+            onClick={() => navigate(left.to)}
+          >
+            <span className="nextprev-button-label">Previous</span>
 
-              <span className="nextprev-button-text">
-                <span>
-                  <BiChevronsLeft
-                    size={24}
-                    style={{ position: "relative", top: 6 }}
-                  />{" "}
-                </span>{" "}
-                <span>{left.text}</span>
-              </span>
-            </button>
-          )}
-        </Box>
+            <span className="nextprev-button-text">
+              <span>
+                <BiChevronsLeft
+                  size={24}
+                  style={{ position: "relative", top: 6 }}
+                />{" "}
+              </span>{" "}
+              <span>{left.text}</span>
+            </span>
+          </button>
+        )}
+      </div>
 
-        <div style={{ width: 20 }} />
+      <div className="nextprev-right">
+        {right && (
+          <button
+            className="nextprev-button right"
+            onClick={() => navigate(right.to)}
+          >
+            <span className="nextprev-button-label">Next</span>
 
-        <Box flex={1} flexBox jEnd alCenter>
-          {right && (
-            <button
-              className="nextprev-button right"
-              onClick={() => navigate(right.to)}
-            >
-              <span className="nextprev-button-label">Next</span>
-
-              <span className="nextprev-button-text">
-                <span className="text">{right.text}</span>
-                <span className="icon">
-                  <BiChevronsRight
-                    size={24}
-                    style={{ position: "relative", top: 6 }}
-                  />{" "}
-                </span>{" "}
-              </span>
-            </button>
-          )}
-        </Box>
-      </Box>
+            <span className="nextprev-button-text">
+              <span className="text">{right.text}</span>
+              <span className="icon">
+                <BiChevronsRight
+                  size={24}
+                  style={{ position: "relative", top: 6 }}
+                />{" "}
+              </span>{" "}
+            </span>
+          </button>
+        )}
+      </div>
     </div>
   );
 };
