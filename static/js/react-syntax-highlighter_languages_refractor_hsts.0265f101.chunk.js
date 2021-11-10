d32fb21d838734112829c347fad251cfe006ea83
Dipesh Rai <raidipesh78@gmail.com>
2021-11-10 19:35:19 +0545

    Updates

--- static/js/react-syntax-highlighter_languages_refractor_hsts.0265f101.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-ui-animate-docs"]=this["webpackJsonpreact-ui-animate-docs"]||[]).push([[55],{235:function(a,s,e){"use strict";function t(a){a.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}a.exports=t,t.displayName="hsts",t.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.0265f101.chunk.js.map
