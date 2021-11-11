4eb7e1df99979d1c9be9298019d3bf512f787368
Dipesh Rai <raidipesh78@gmail.com>
2021-11-11 21:35:32 +0545

    AlertInfo changed

--- src/app/commons/alertInfo/index.js
+++ src/app/commons/alertInfo/index.js
@@ -10,8 +10,23 @@ export const AlertInfo = () => {
 
         <div className="alert-info-title-text">INFO</div>
       </div>
-      Due to time constraint, we are unable to complete this documentation. We
-      are working on this documentation. We will update as soon as possible.
+      This is a documentation website of React UI Animate stable release.
+      <br />
+      <br />
+      If you find any bugs or issue, then you can submit an issue{" "}
+      <span
+        onClick={() =>
+          window.open("https://github.com/dipeshrai123/react-ui-animate/issues")
+        }
+        style={{
+          borderBottom: "1px solid #3399ff",
+          color: "#353535",
+          cursor: "pointer",
+        }}
+      >
+        here in this link
+      </span>{" "}
+      .
     </div>
   );
 };
